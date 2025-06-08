efd661cd15f06fad70d7d75fdeee70d07b5c874e
Vladislav Kulikov <vladislav.kulikov@sravni.net>
2025-06-08 03:37:02 +0300

    refactor: remove nginx and use direct backend connection

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({ 
-  baseURL: 'http://62.109.17.167/api',
+  baseURL: 'http://62.109.17.167:3001',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
